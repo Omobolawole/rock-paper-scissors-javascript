456a2cf397ac065485f40fff01cc17e679cceb14
Bolawole <andubolawole@gmail.com>
2022-02-23 10:16:20 +0100

    First draft of RPC UI

--- script.js
+++ script.js
@@ -1,69 +1,100 @@
-//Declare variables for the moves and initial players' scores
-const moves = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
+let playedRounds = 0;
+const moves = ['rock', 'paper', 'scissors'];
 
-//Declare a function to get the number of rounds the player wants
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const results = document.querySelector('#display-results p');
+const whatRound = document.querySelector('.what-round');
+const playerMove = document.querySelector('.player-move');
+const computerMove = document.querySelector('.computer-move');
+const scores = document.querySelector('.scores');
+const roundResult = document.querySelector('.round-result');
+const finalScores = document.querySelector('.final-scores');
+const gameResult = document.querySelector('.game-result');
+
 function getRounds() {
-    while (true) {
-        rounds = Number(prompt('How many rounds?', ' '));
-        if (typeof rounds === 'number') {
-            return rounds;
-        } 
-    } 
+    rounds = document.querySelector('#input-rounds');
+    return Number(rounds.value);
 }
 
-//Declare a function to get the player's move at each round
-function playerPlay() {
-    while (true) {
-        move = (prompt('Rock, Paper, Scissors?', ' ')).toLowerCase();
-        if (moves.includes(move)) {
-            return move;
-        }
-    }  
-}
-
-//Declare a function to get the computer's move at each round
 function computerPlay() {
     randomMove = moves[Math.floor(Math.random()*moves.length)];
     return randomMove;
 }
 
-//Declare a function to determine what move beats what
 function beats(moveOne, moveTwo) {
     if ((moveOne === 'rock' && moveTwo === 'scissors') || 
         (moveOne === 'scissors' && moveTwo === 'paper') ||
         (moveOne === 'paper' && moveTwo === 'rock')) {
-            return ('Player wins')
+        return ('Player wins')
     } else if ((moveOne === 'scissors' && moveTwo === 'rock') || 
         (moveOne === 'paper' && moveTwo === 'scissors') ||
         (moveOne === 'rock' && moveTwo === 'paper')) {
-            return ('Computer wins')
+        return ('Computer wins')
     } else {
-            return ('Both tie')
+        return ('Both tie')
     }
 }
 
-//Declare a function to display the players' scores
 function displayScores(pScore, cScore) {
-    console.log(`Your score: ${pScore}`);
-    console.log(`Computer's score: ${cScore}`);
+    scores.textContent = `Your score: ${pScore} || Computer's score: ${cScore}`;
 }
 
-//Declare a function to play one round of the game
+function displayFinalScores() {
+    finalScores.textContent = '--Final Scores--'
+    displayScores(playerScore, computerScore);
+    if (playerScore > computerScore) {
+        gameResult.textContent = 'You have won the game!';
+    } else if (computerScore > playerScore) {
+        gameResult.textContent = 'You have lost the game!';
+    } else {
+        gameResult.textContent = 'The game is tied!';
+    }
+}
+
+function setGameOver() {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+    rounds.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Play Again';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    playedRounds = 0;
+    for(let result of results) {
+        result.textContent = '';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+    rounds.disabled = false;
+    rounds.value = '';
+    rounds.focus();
+}
+
 function playRound(playerSelection, computerSelection) {
-    console.log(`You played ${playerSelection}`);
-    console.log(`Computer played ${computerSelection}`);
+
+    playerMove.textContent = `You played ${playerSelection}`;
+    computerMove.textContent = `Computer played ${computerSelection}`;
 
     if (beats(playerSelection, computerSelection) === 'Player wins') {
         playerScore += 1;
         displayScores(playerScore, computerScore);
         if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            console.log('You win! Rock beats Scissors');
+            roundResult.textContent = 'You win! Rock beats Scissors';
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            console.log('You win! Scissors beats Paper');
+            roundResult.textContent = 'You win! Scissors beats Paper';
         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            console.log('You win! Paper beats Rock');
+            roundResult.textContent = 'You win! Paper beats Rock';
         }
     }
     
@@ -71,42 +102,40 @@ function playRound(playerSelection, computerSelection) {
         computerScore += 1;
         displayScores(playerScore, computerScore);
         if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            console.log('You lose! Rock beats Scissors');
+            roundResult.textContent = 'You lose! Rock beats Scissors';
         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            console.log('You lose! Scissors beats Paper');
+            roundResult.textContent = 'You lose! Scissors beats Paper';
         } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            console.log('You lose! Paper beats Rock');
+            roundResult.textContent = 'You lose! Paper beats Rock';
         }
     }
     
     if (beats(playerSelection, computerSelection) === 'Both tie') { 
             displayScores(playerScore, computerScore);     
-            console.log('It\'s a tie!');
+            roundResult.textContent = 'It\'s a tie!';
     }
+    playedRounds += 1;
 }
 
-//Declare a function to play the entire game, depending on number of rounds entered 
-//THis function also displays the final scores and whether the player wins, loses or ties the game
-function playGame() {
+function playGame(event) {
+    event.preventDefault();
     let gameRounds = getRounds();
-    
+
     for(let i = 1; i <= gameRounds; i++) {
-        console.log(`--Round ${i}--`);
-        let playerSelection = playerPlay();
+        whatRound.textContent = `--Round ${i}--${gameRounds}`;
+
+        let target = event.target;
+        let playerSelection = target.id;
         let computerSelection = computerPlay();
+        if (playedRounds > gameRounds) {
+            setGameOver();
+        }      
         playRound(playerSelection, computerSelection);
     }
-
-    console.log('--Final Scores--')
-    displayScores(playerScore, computerScore);
-    if (playerScore > computerScore) {
-        console.log('You have won the game!');
-    } else if (computerScore > playerScore) {
-        console.log('You have lost the game!');
-    } else {
-        console.log('The game is tied!');
-    }
+    displayFinalScores();
 }
 
-//Call the playGame() function to start playing the game
-playGame();
+const buttons = document.querySelectorAll('.buttons');
+buttons.forEach(function(button) {
+    button.addEventListener('click', playGame);
+});
